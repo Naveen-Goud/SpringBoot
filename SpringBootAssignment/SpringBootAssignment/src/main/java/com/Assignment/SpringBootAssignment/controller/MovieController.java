440d7739082d54c561aaac11b57b36e4af3fe4f6
naveen goud <naveengoud7799@gmail.com>
2023-06-14 11:37:09 +0530

    Spring boot assignment review

--- SpringBootAssignment/SpringBootAssignment/src/main/java/com/Assignment/SpringBootAssignment/controller/MovieController.java
+++ SpringBootAssignment/SpringBootAssignment/src/main/java/com/Assignment/SpringBootAssignment/controller/MovieController.java
@@ -3,7 +3,6 @@ package com.Assignment.SpringBootAssignment.controller;
 import com.Assignment.SpringBootAssignment.entity.Movie;
 import com.Assignment.SpringBootAssignment.services.MovieServices;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,42 +19,21 @@ public class MovieController {
         movieServices=movieService;
     }
 
-    @RequestMapping("/list")
-    public String listMovies(Model theModel) {
 
-        List<Movie> theMovies =  movieServices.moviesName();
-        theModel.addAttribute("movies", theMovies);
 
-        return "list-movies";
-
-    }
-    @GetMapping("/showFormForAdd")
-    public String showFormForAdd(Model theModel) {
-
-        // create model attribute to bind form data
-        Movie theMovie = new Movie();
-
-        theModel.addAttribute("movies", theMovie);
-
-        return "movie-form";
-    }
-
-    @GetMapping("/{id}")
+   @GetMapping("/{id}")
     public Movie movieName(@PathVariable int id){
         return movieServices.movieName(id);
     }
-//
-//    @GetMapping("/list")
-//    public List<Movie > moviesName(){
-//        return movieServices.moviesName();
-//    }
 
-    @PostMapping("/saveMovies")
-    public String saveMovies(@ModelAttribute("movies")   Movie movie){
-         movieServices.save(movie);
+    @GetMapping("/list")
+    public List<Movie > moviesName(){
+        return movieServices.moviesName();
+    }
 
-         return "redirect:/movies/list";
-
+    @PostMapping("/")
+    public Movie postMovie(@RequestBody Movie movie){
+        return movieServices.save(movie);
     }
 
     @PutMapping("/movies")
